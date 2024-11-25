9e1069971ba12735fd8ae7192d96dc65e04cae77
akhil <akhil@Akhilesh>
2024-11-25 12:15:50 +0530

    commit 3

---
+++ GithubPrograming/src/ArrayBased/A001_PrintOneToTenWithoutLoop.java
@@ -0,0 +1,19 @@
+package ArrayBased;
+
+public class A001_PrintOneToTenWithoutLoop {
+
+	public static void display(int n) {
+		if(n>10) {
+			return;
+		}else {
+			System.out.println(n++);
+			display(n);
+		}		
+	}
+	
+	public static void main(String[] args) {
+		
+        display(1);
+	}
+
+}
